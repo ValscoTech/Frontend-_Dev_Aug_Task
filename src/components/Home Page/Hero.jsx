f86e88c4e230cef4535a82253e5e9061c669a937
Pratyush Sinha <pratisinha@gmail.com>
2024-09-13 21:37:33 +0530

    Change in the Hero Section

--- src/components/Home Page/Hero.jsx
+++ src/components/Home Page/Hero.jsx
@@ -1,5 +1,3 @@
-// Bootstrap
-
 import Pencil from '../../assets/guywithpencil.svg'
 import DownRightArrow from "../../assets/arrowdownright.svg"
 
@@ -19,37 +17,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
-
-// Tailwindcss
-
-// import Pencil from '../../assets/guywithpencil.svg';
-// import DownRightArrow from "../../assets/arrowdownright.svg";
-
-// const Hero = () => {
-//     return (
-//         <section className="container bg-black text-white text-center flex items-center h-screen w-full">
-//             <div className="w-full px-8 m-5" style={{ fontFamily: "ClashDisplayVariable" }}>
-//                 <div className="flex flex-col md:flex-row items-center justify-between h-full">
-//                     {/* Left Column - Text and Button */}
-//                     <div className="md:w-1/2 text-left">
-//                         <h1 className="display-4 md:display-3 font-bold leading-tight my-4" >
-//                             Want to earn some <span style={{ fontWeight: "bold", color: "yellow" }} >Side hustle</span> from all that <span style={{ fontWeight: "bold", color: "yellow" }}>Hardwork</span> you do in making <span style={{ fontWeight: "bold", color: "yellow" }}>Notes</span>?
-//                         </h1>
-//                         <button className="mt-6 text-white text-lg font-semibold px-6 py-3 rounded-lg flex items-center" style={{ backgroundColor: "#A883C4", borderRadius: "15px" }}>
-//                             <img src={DownRightArrow} className="h-8 mr-3" alt="Down Right Arrow" style={{ height: "2.5rem" }} />
-//                             RENT NOTES
-//                         </button>
-//                     </div>
-
-//                     {/* Right Column - Image */}
-//                     <div className="mt-10 md:mt-0 md:w-1/2 flex justify-center my-3">
-//                         <img src={Pencil} alt="Person with Pencil" className="max-w-full h-auto" />
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// };
-
-// export default Hero;
